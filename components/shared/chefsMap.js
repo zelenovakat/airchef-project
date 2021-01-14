db515f137b15196a0168db31ed1e71f3eba9d818
zelenovakat <git@github.com:zelenovakat/github-demo.git>
2021-01-14 14:27:33 +0100

    link for chefs

--- components/shared/chefsMap.js
+++ components/shared/chefsMap.js
@@ -1,16 +1,22 @@
 import { Chefs } from "./chefs"
 import styled from "styled-components"
 import { grey } from "../colors"
+import Link from "next/link"
 
 const ChefsMap = () => {
   const mappedChefs = Chefs.map((typesOfName, i) => {
     return (
       <MainDiv key={i}>
-        <img src={typesOfName.profileImg} alt="" />
-        <div>
-          <h3>{typesOfName.name}</h3>
-          <h5>{typesOfName.title}</h5>
-        </div>
+        <MainLink href={typesOfName.link}>
+          <a>
+            <img src={typesOfName.profileImg} alt="" />
+            <div>
+              <h3>{typesOfName.name}</h3>
+              <p>{typesOfName.titleRestaurant}</p>
+              <p>{typesOfName.titleFood}</p>
+            </div>
+          </a>
+        </MainLink>
       </MainDiv>
     )
   })
@@ -20,6 +26,12 @@ const ChefsMap = () => {
 
 export default ChefsMap
 
+const MainLink = styled(Link)`
+  a {
+    text-decoration: none;
+  }
+`
+
 const MainDiv = styled.div`
   border-radius: 10px;
   box-shadow: ${grey} 0.2em 0.3em 0.3em 0.3em;
@@ -27,10 +39,18 @@ const MainDiv = styled.div`
   img {
     width: 315px;
     height: 315px;
-    position: relative;
     overflow: hidden;
     object-fit: cover;
     border-radius: 10px 10px 0 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  h3,
+  p {
+    margin: 0;
   }
 `
 const MappedDiv = styled.div`
