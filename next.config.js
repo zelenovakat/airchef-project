f784c0831908ee80df7e266e73808cb21ffba3e3
zelenovakat <kate@clvlab.com>
2021-04-15 11:05:17 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
-// const withImages = require("next-images")
-// module.exports = withImages()
-
-module.exports = {
-  images: {
-    domains: ["assets.vercel.com"],
-    future: { webpack5: true },
-  },
-}
+const withImages = require("next-images")
+module.exports = withImages()
